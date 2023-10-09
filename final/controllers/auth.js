58c07eee5fae5bb940e0a4ed965340da02841f21
smajaas <smajaas@gmail.com>
2023-10-09 10:20:30 +0530

    mongoose middleware

--- final/controllers/auth.js
+++ final/controllers/auth.js
@@ -4,19 +4,12 @@ const { BadRequestError } = require('../errors');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
   //Optional error
   //const { name, email, password } = req.body;
   // if (!name || !email || !password) {
   //   throw new BadRequestError('Please provide name,email and password');
   // }
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
