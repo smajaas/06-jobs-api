8919feac61cecfd3d3b570a7e2e0a124debdd36e
smajaas <smajaas@gmail.com>
2023-10-15 23:13:59 +0530

    register controller

--- final/controllers/auth.js
+++ final/controllers/auth.js
@@ -1,19 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  //Optional error
-  //const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError('Please provide name,email and password');
-  // }
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
-  res.send('Login user');
+  res.send('login user');
 };
 
 module.exports = { register, login };
