a4dcf1d416fad76bee7f1022a154bc7b484b78f4
smajaas <smajaas@gmail.com>
2023-10-30 18:10:47 +0530

    done

--- final/app.js
+++ final/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const { xss } = require('express-xss-sanitizer');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -17,8 +24,20 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+  })
+);
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
